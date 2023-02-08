dc0121c29e991923e7bb58137b09169c084e9dda
Okan <okanacioglu@gmail.com>
2023-02-08 22:20:21 +0300

    Shared-layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Error from '../pages/Error'
-import Dashboard from '../pages/Dashboard'
 import Register from '../pages/Register'
 import Landing from '../pages/Landing'
 
-export { Error, Dashboard, Register, Landing }
+export { Error, Register, Landing }
