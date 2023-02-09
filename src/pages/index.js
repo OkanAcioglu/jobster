dfe7c0dc70723a19c3a4e2b4ffacb6d28936c1da
Okan Acıoğlu <okanacioglu@gmail.com>
2023-02-09 13:23:37 +0300

    protected-route

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Error from '../pages/Error'
 import Register from '../pages/Register'
 import Landing from '../pages/Landing'
+import ProtectedRoute from '../pages/ProtectedRoute'
 
-export { Error, Register, Landing }
+export { Error, Register, Landing, ProtectedRoute }
