d646670527f826441bd38a8bb8f40f8adf1348a3
Okan Acıoğlu <okanacioglu@gmail.com>
2023-02-09 12:27:56 +0300

    logout-functionality

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -53,6 +53,11 @@ const userSlice = createSlice({
     toggleSidebar: (state) => {
       state.isSidebarOpen = !state.isSidebarOpen
     },
+    logoutUser: (state) => {
+      state.user = null
+      state.isSidebarOpen = false
+      removeUserFromLocalStorage()
+    },
   },
   extraReducers: {
     [registerUser.pending]: (state) => {
@@ -85,5 +90,5 @@ const userSlice = createSlice({
     },
   },
 })
-export const { toggleSidebar } = userSlice.actions
+export const { toggleSidebar, logoutUser } = userSlice.actions
 export default userSlice.reducer
