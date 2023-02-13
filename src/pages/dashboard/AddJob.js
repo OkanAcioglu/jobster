f4e9e64c03db731a35d497a7f4404c65bc0b612a
Okan Acıoğlu <okanacioglu@gmail.com>
2023-02-13 14:20:03 +0300

    edit-job-request

--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -6,6 +6,7 @@ import {
   handleChange,
   clearValues,
   createJob,
+  editJob,
 } from '../../features/job/jobSlice'
 import { useEffect } from 'react'
 
@@ -31,6 +32,15 @@ const AddJob = () => {
 
     if (!position || !company || !jobLocation) {
       toast.error('Please Fill Out All Fields')
+      return
+    }
+    if (isEditing) {
+      dispatch(
+        editJob({
+          jobId: editJobId,
+          job: { position, company, jobLocation, jobType, status },
+        })
+      )
       return
     }
     dispatch(createJob({ position, company, jobLocation, jobType, status }))
