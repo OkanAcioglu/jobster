d5faf4bcc127f21de5dfe95fb14a05494b93d623
Okan Acıoğlu <okanacioglu@gmail.com>
2023-02-15 14:29:00 +0300

    change-page-functionality

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -66,6 +66,9 @@ const allJobsSlice = createSlice({
     clearFilters: (state) => {
       return { ...state, ...initialFiltersState }
     },
+    changePage: (state, { payload }) => {
+      state.page = payload
+    },
   },
   extraReducers: {
     [getAllJobs.pending]: (state) => {
@@ -97,5 +100,10 @@ const allJobsSlice = createSlice({
 })
 
 export default allJobsSlice.reducer
-export const { showLoading, hideLoading, handleChange, clearFilters } =
-  allJobsSlice.actions
+export const {
+  showLoading,
+  hideLoading,
+  handleChange,
+  clearFilters,
+  changePage,
+} = allJobsSlice.actions
